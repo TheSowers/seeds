c94f7fb53b5207143d49587c39f5d640628b489a
hyoeun kim <73044080+hyosari-kim@users.noreply.github.com>
2023-02-25 18:41:54 +0900

    add: 프론트 마크업 (#3)
    * feat: disqus 추가

--- apps/last-train/src/routes/+page.svelte
+++ apps/last-train/src/routes/+page.svelte
@@ -1,9 +1,106 @@
-<script>
-	import { Alert } from 'flowbite-svelte';
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { Button, P } from 'flowbite-svelte';
+	import Select from 'svelte-select';
+
+	let items = [
+		{ value: '강남역', label: '강남역' },
+		{ value: '선릉역', label: '선릉역' },
+		{ value: '합정역', label: '합정역' },
+		{ value: '시청역', label: '시청역' },
+		{ value: '을지로3가역', label: '을지로3가역' }
+	];
+
+	let departure = '';
+	let arrival = '';
+
+	let handleInput = (fn: Function) => (e: CustomEvent<{ value: string; label: string }>) => {
+		fn(e.detail.value);
+	};
+
+	let setDeparture = (v: string) => (departure = v);
+
+	let setArrival = (v: string) => (arrival = v);
+
+	let handleSubmit = () => {
+		let query = new URLSearchParams();
+		query.set('departure', departure);
+		query.set('arrival', arrival);
+
+		goto(`\last-time?${query.toString()}`);
+	};
 </script>
 
-<div class="p-8">
-	<Alert>
-		<span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
-	</Alert>
-</div>
+<form class="w-full px-4" on:submit|preventDefault={handleSubmit}>
+	<div class="py-6 w-full mb-4 flex flex-col gap-7">
+		<Select
+			{items}
+			name="departure"
+			placeholder="출발역 (2호선만 가능해요)"
+			class="h-14 !text-gray-900 !bg-gray-50 !border-gray-300"
+			inputStyles="--tw-ring-color: transparent;"
+			on:change={handleInput(setDeparture)}
+		>
+			<div slot="prepend" class="mr-1">
+				<svg class="w-6 h-6" fill="#6b7280" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
+					><path
+						fill-rule="evenodd"
+						d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+						clip-rule="evenodd"
+					/></svg
+				>
+			</div>
+
+			<div slot="empty" class="p-3 text-center">역이 없습니다.</div>
+
+			<div slot="item" let:item>
+				{item.label}
+				<span class="rounded-full bg-train-2 text-white px-1 text-sm align-text-top">2</span>
+			</div>
+
+			<div slot="selection" let:selection>
+				{selection.label}
+				<span class="rounded-full bg-train-2 text-white px-1 text-sm align-text-top">2</span>
+			</div>
+		</Select>
+		<Select
+			{items}
+			name="arrival"
+			placeholder="도착역"
+			class="h-14 !text-gray-900 !bg-gray-50 !border-gray-300"
+			inputStyles="--tw-ring-color: transparent;"
+			on:change={handleInput(setArrival)}
+		>
+			<div slot="prepend" class="mr-1">
+				<svg class="w-6 h-6" fill="#6b7280" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
+					><path
+						fill-rule="evenodd"
+						d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+						clip-rule="evenodd"
+					/></svg
+				>
+			</div>
+
+			<div slot="empty" class="p-3 text-center">역이 없습니다.</div>
+
+			<div slot="item" let:item>
+				{item.label}
+				<span class="rounded-full bg-train-2 text-white px-1 text-sm align-text-top">2</span>
+			</div>
+
+			<div slot="selection" let:selection>
+				{selection.label}
+				<span class="rounded-full bg-train-2 text-white px-1 text-sm align-text-top">2</span>
+			</div>
+		</Select>
+	</div>
+	<Button
+		class="w-full"
+		color="dark"
+		size="xl"
+		type="submit"
+		disabled={departure == '' || arrival == ''}
+	>
+		<P size="base" color="text-white" space="wide">막차 몇시?</P>
+	</Button>
+</form>
---
+++ apps/last-train/src/routes/last-time/+page.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { format } from 'date-fns';
+
+	export let data: PageData;
+
+	console.log(data);
+
+	let time = format(data?.lastTime, 'HH시 mm분');
+</script>
+
+<div class="w-full overflow-y-auto">
+	<h1 class="px-8 mt-12">
+		<span class="text-2xl font-bold">{data?.departure.name} </span>
+		<span class="text-lg">에서</span>
+		<br />
+		<span class="text-3xl font-bold">{time}</span>
+		<span class="text-lg">열차를 타야해요!</span>
+	</h1>
+	<div class="mt-10 bg-gray-100 mx-4 rounded-lg p-4">
+		<div class="flex flex-col">
+			<div class="pl-8 border-train-2 border-l-4 relative mx-4 h-[130px]">
+				<div class="absolute rounded-full top-0 -left-[0.85rem] bg-train-2 text-white px-2">2</div>
+				<div class="flex flex-col w-full h-full justify-between">
+					<div class="bg-gray-50 px-4 py-2 rounded-lg">
+						<div class="font-bold text-lg">강남역 승차</div>
+						<div class="text-red-600 text-sm">00: 00</div>
+					</div>
+					<div class="px-4 text-gray-500 text-sm">환승 00 분 소요</div>
+				</div>
+			</div>
+			<div class="pl-8 border-train-1 border-l-4 relative mx-4 h-[130px]">
+				<div class="absolute rounded-full -top-0 -left-[0.85rem] bg-train-1 text-white px-2">1</div>
+				<div class="flex flex-col w-full h-full justify-between">
+					<div class="bg-gray-50 px-4 py-2 rounded-lg">
+						<div class="font-bold text-lg">신도림역(인천역행) 환승</div>
+						<div class="text-red-600 text-sm">00: 00</div>
+					</div>
+				</div>
+			</div>
+			<div class="pl-8 mx-4 relative w-full">
+				<div
+					class="absolute rounded-full -top-0 -left-[0.65rem] bg-train-1 text-white text-[10px] p-1"
+				>
+					도착
+				</div>
+				<div class="px-4 text-gray-500 text-sm">00: 00</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div id="disqus_thread" class="w-full h-96 mt-10" />
